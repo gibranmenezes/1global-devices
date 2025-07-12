08726709e983f1897bb951cde27daf8f9c3cb27f
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-12 18:03:49 -0300

    feat: implementig UseCases - method create and findById

---
+++ api/src/main/java/one/globa/api/application/service/DeviceUserCaseService.java
@@ -0,0 +1,53 @@
+package one.globa.api.application.service;
+
+import one.globa.api.application.port.in.DeviceUseCase;
+import one.globa.api.application.port.out.DeviceRepository;
+import one.globa.api.domain.enums.State;
+import one.globa.api.domain.model.Device;
+
+import java.util.List;
+
+public class DeviceUserCaseService implements DeviceUseCase {
+
+    private final DeviceRepository deviceRepository;
+
+    public DeviceUserCaseService(DeviceRepository deviceRepository) {
+        this.deviceRepository = deviceRepository;
+    }
+
+
+    @Override
+    public Device createDevice(String name, String brand) {
+        Device device = new Device(name, brand);
+        return deviceRepository.save(device);
+    }
+
+    @Override
+    public Device getDeviceById(Long id) {
+        Device device = deviceRepository.findById(id);
+        if (device == null) {
+            throw new RuntimeException(String.format("Device with id %s not found", id));
+        }
+        return device;
+    }
+
+    @Override
+    public List<Device> getAllDevices(String brand, State state) {
+        return List.of();
+    }
+
+    @Override
+    public void updateDevice(Long id, String name, String brand, State state) {
+
+    }
+
+    @Override
+    public void partiallyUpdateDevice(Long id, String name, String brand, State state) {
+
+    }
+
+    @Override
+    public void deleteDevice(Long id) {
+
+    }
+}
