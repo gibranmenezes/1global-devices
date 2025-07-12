8feaffc7ff04222edfdce46a9d19ffa7bc148f73
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-12 17:24:25 -0300

    feat: implementing find methods

--- api/src/main/java/one/globa/api/adapter/out/persistence/DeviceRespositoryAdapter.java
+++ api/src/main/java/one/globa/api/adapter/out/persistence/DeviceRespositoryAdapter.java
@@ -1,37 +1,50 @@
 package one.globa.api.adapter.out.persistence;
 
+import lombok.RequiredArgsConstructor;
+import one.globa.api.adapter.out.DeviceMapper;
+import one.globa.api.adapter.out.entity.JpaDeviceEntity;
 import one.globa.api.application.port.out.DeviceRepository;
 import one.globa.api.domain.enums.State;
 import one.globa.api.domain.model.Device;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Repository
+@RequiredArgsConstructor
 public class DeviceRespositoryAdapter implements DeviceRepository {
+
+    private final JpaDeviceRepository jpaDeviceRepository;
+
+    @Autowired
+    private DeviceMapper deviceMapper;
+
     @Override
-    public Device save(String name, String brand, String state) {
-        return null;
+    public Device save(Device device) {
+        JpaDeviceEntity entity = deviceMapper.fromDeviceToJpaDeviceEntity(device);
+        JpaDeviceEntity savedEntity = jpaDeviceRepository.save(entity);
+        return deviceMapper.fromJpaDeviceEntityToDevice(savedEntity);
     }
 
     @Override
     public Device findById(Long id) {
-        return null;
+        var optionalEntity = jpaDeviceRepository.findById(id);
+        return optionalEntity.map(jpaDeviceEntity ->
+                deviceMapper.fromJpaDeviceEntityToDevice(jpaDeviceEntity)).orElse(null);
     }
 
     @Override
     public List<Device> findAll(String brand, State state) {
-        return List.of();
+        return  null;
     }
 
     @Override
-    public Device update(Long id, String name, String brand, State state) {
-        return null;
-    }
+    public void update(Long id, Device device) {
 
-    @Override
-    public Device patchDevice(Long id, String name, String brand, State state) {
-        return null;
-    }
 
+    }
     @Override
     public void delete(Long id) {
 
--- api/src/main/java/one/globa/api/adapter/out/persistence/JpaDeviceRepository.java
+++ api/src/main/java/one/globa/api/adapter/out/persistence/JpaDeviceRepository.java
@@ -2,8 +2,19 @@ package one.globa.api.adapter.out.persistence;
 
 import one.globa.api.adapter.out.entity.JpaDeviceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface JpaDeviceRepository extends JpaRepository<JpaDeviceEntity, Long> {
 
+    JpaDeviceEntity findById(long id);
+
+    @Query("SELECT d FROM JpaDeviceEntity d WHERE (:brand IS NULL OR d.brand = :brand) AND (:state IS NULL OR d.state = :state)")
+    List<JpaDeviceEntity> findAllByBrandOrState(@Param("brand") String brand, @Param("state") String state);
 
 }
+
+
+
