ace435498cdb175dc4ccd63931ff57fb449afb15
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-12 18:08:31 -0300

    feat: creating controller

---
+++ api/src/main/java/one/globa/api/adapter/in/DeviceController.java
@@ -0,0 +1,23 @@
+package one.globa.api.adapter.in;
+
+import lombok.RequiredArgsConstructor;
+import one.globa.api.adapter.out.DeviceMapper;
+import one.globa.api.application.port.in.DeviceUseCase;
+import one.globa.api.presentation.dto.DeviceRequestDTO;
+import one.globa.api.presentation.dto.DeviceResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/devices")
+@RequiredArgsConstructor
+public class DeviceController {
+    private final DeviceUseCase deviceUseCase;
+    private final DeviceMapper deviceMapper;
+
+    public ResponseEntity<DeviceResponseDTO> registerDevice(DeviceRequestDTO deviceRequestDTO) {
+        var device = deviceUseCase.createDevice(deviceRequestDTO.name(), deviceRequestDTO.brand());
+        return ResponseEntity.ok(deviceMapper.fromDeviceToDeviceResponseDTO(device));
+    }
+}
