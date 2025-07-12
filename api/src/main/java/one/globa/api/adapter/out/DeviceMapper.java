ef68b0dc2bfa5bcb73907f247f2f165549c46331
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-12 18:08:03 -0300

    feat: adding method to map from devide to DTO

---
+++ api/src/main/java/one/globa/api/adapter/out/DeviceMapper.java
@@ -0,0 +1,23 @@
+package one.globa.api.adapter.out;
+
+import one.globa.api.adapter.out.entity.JpaDeviceEntity;
+import one.globa.api.domain.model.Device;
+import one.globa.api.presentation.dto.DeviceResponseDTO;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface DeviceMapper {
+
+    @Mapping(target = "name")
+    @Mapping(target = "brand")
+    @Mapping(target = "state")
+    @Mapping(target = "createdAt")
+    JpaDeviceEntity fromDeviceToJpaDeviceEntity(Device device);
+
+    Device fromJpaDeviceEntityToDevice(JpaDeviceEntity jpaDeviceEntity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDomain(Device device, @MappingTarget JpaDeviceEntity entity);
+
+    DeviceResponseDTO fromDeviceToDeviceResponseDTO(Device device);
+}
