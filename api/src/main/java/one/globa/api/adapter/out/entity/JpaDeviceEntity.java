eb267f27208890588e20dbcdd282b7612343281f
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-12 21:54:19 -0300

    refact: changing type of atribute State

--- api/src/main/java/one/globa/api/adapter/out/entity/JpaDeviceEntity.java
+++ api/src/main/java/one/globa/api/adapter/out/entity/JpaDeviceEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
-import one.globa.api.domain.enums.State;
 
 import java.time.LocalDateTime;
 
