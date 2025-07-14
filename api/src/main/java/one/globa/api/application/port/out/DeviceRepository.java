bc4e75fbc42e67157c3ec5a636d279b591f5c76f
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-14 08:14:02 -0300

    refact: changing signature method

--- api/src/main/java/one/globa/api/application/port/out/DeviceRepository.java
+++ api/src/main/java/one/globa/api/application/port/out/DeviceRepository.java
@@ -2,6 +2,7 @@ package one.globa.api.application.port.out;
 
 import one.globa.api.domain.enums.State;
 import one.globa.api.domain.model.Device;
+import one.globa.api.presentation.dto.PaginatedResponse;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ public interface DeviceRepository {
 
     Device save(Device device);
     Device findById(Long id);
-    List<Device> findAll(String brand, State state);
+    PaginatedResponse<Device> findAll(String brand, State state, int page, int size);
     void update(Long id, Device device);
     void delete(Long id);
 }
