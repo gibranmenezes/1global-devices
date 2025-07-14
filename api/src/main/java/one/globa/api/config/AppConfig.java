7e39a86a09b508a06fd5b8663fa92584acb262be
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-14 07:53:05 -0300

    feat: adding validation class in appconfig

--- api/src/main/java/one/globa/api/config/AppConfig.java
+++ api/src/main/java/one/globa/api/config/AppConfig.java
@@ -3,14 +3,27 @@ package one.globa.api.config;
 import one.globa.api.application.port.in.DeviceUseCase;
 import one.globa.api.application.port.out.DeviceRepository;
 import one.globa.api.application.service.DeviceUserCaseService;
+import one.globa.api.application.validation.registration.DeviceRegistrationValidator;
+import one.globa.api.application.validation.registration.NameBrandValidation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import java.util.List;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public DeviceUseCase deviceUseCase(DeviceRepository deviceRepository) {
-        return new DeviceUserCaseService(deviceRepository);
+    public DeviceUseCase deviceUseCase(DeviceRepository deviceRepository,
+                                       List<DeviceRegistrationValidator> deviceRegistrationValidators) {
+        return new DeviceUserCaseService(deviceRepository, deviceRegistrationValidators);
+    }
+
+    /*Validators for device registering*/
+    @Bean
+    @Order(1)
+    public NameBrandValidation nameBrandCreationValidation() {
+        return new NameBrandValidation();
     }
 }
