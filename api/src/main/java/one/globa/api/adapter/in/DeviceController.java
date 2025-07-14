b102fc87fa37132f8f8f12a0c8484af39205da9e
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-14 08:20:37 -0300

    feat: implementing device searching endpoint

--- api/src/main/java/one/globa/api/adapter/in/DeviceController.java
+++ api/src/main/java/one/globa/api/adapter/in/DeviceController.java
@@ -29,6 +29,32 @@ public class DeviceController {
                 .getResponseEntity();
     }
 
+    @GetMapping
+    public ResponseEntity<AppResponse<List<DeviceResponseDTO>>> getDevices(
+            @RequestParam(required = false) String brand,
+            @RequestParam(required = false) String state,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size)  {
 
+        var validState = state != null && !state.isBlank() ? State.valueOf(state) : null;
+
+        PaginatedResponse<Device> paginatedDevices = deviceUseCase
+                .getAllDevices(brand, validState, page, size);
+
+        List<DeviceResponseDTO> deviceResponseDTOs = paginatedDevices.getContent().stream()
+                .map(deviceMapper::fromDeviceToDeviceResponseDTO)
+                .toList();
+
+        AppResponse<List<DeviceResponseDTO>> finalAppResponse = AppResponse.ok("Devices retrieved successfully", deviceResponseDTOs)
+                .buildParametersPagination(
+                        paginatedDevices.getPageNumber(),
+                        paginatedDevices.getPageSize(),
+                        paginatedDevices.getTotalElements(),
+                        paginatedDevices.getTotalPages()
+                );
+
+        return finalAppResponse.getResponseEntity();
+
+    }
 
 }
