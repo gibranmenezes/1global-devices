9f148cca1adf2fed99798bd6340df30284edbab9
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-14 08:12:18 -0300

    refact: adding pageable parameter

--- api/src/main/java/one/globa/api/adapter/out/persistence/JpaDeviceRepository.java
+++ api/src/main/java/one/globa/api/adapter/out/persistence/JpaDeviceRepository.java
@@ -1,18 +1,18 @@
 package one.globa.api.adapter.out.persistence;
 
 import one.globa.api.adapter.out.entity.JpaDeviceEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface JpaDeviceRepository extends JpaRepository<JpaDeviceEntity, Long> {
 
     JpaDeviceEntity findById(long id);
 
     @Query("SELECT d FROM JpaDeviceEntity d WHERE (:brand IS NULL OR d.brand = :brand) AND (:state IS NULL OR d.state = :state)")
-    List<JpaDeviceEntity> findAllByBrandOrState(@Param("brand") String brand, @Param("state") String state);
+    Page<JpaDeviceEntity> findAllByBrandOrState(@Param("brand") String brand, @Param("state") String state, Pageable pageable);
 
 }
 
