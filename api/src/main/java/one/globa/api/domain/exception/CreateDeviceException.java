33d7234fd04581a45f47d6c03fc83946ca606f33
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-14 07:53:33 -0300

    feat: creating custom exception for de device registration

---
+++ api/src/main/java/one/globa/api/domain/exception/CreateDeviceException.java
@@ -0,0 +1,8 @@
+package one.globa.api.domain.exception;
+
+public class CreateDeviceException extends RuntimeException {
+
+    public CreateDeviceException(String message) {
+        super(message);
+    }
+}
