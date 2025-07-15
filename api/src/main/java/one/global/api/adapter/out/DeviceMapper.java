50174b0d73dc95b40bf5e3844fbc062f77c7478a
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-15 09:26:47 -0300

    feat: adding object factory method in mapper

--- api/src/main/java/one/global/api/adapter/out/DeviceMapper.java
+++ api/src/main/java/one/global/api/adapter/out/DeviceMapper.java
@@ -2,7 +2,7 @@ package one.global.api.adapter.out;
 
 import one.global.api.adapter.out.entity.JpaDeviceEntity;
 import one.global.api.domain.model.Device;
-import one.global.api.presentation.dto.DeviceResponseDTO;
+import one.global.api.web.dto.DeviceResponseDTO;
 import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
@@ -19,11 +19,26 @@ public interface DeviceMapper {
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntityFromDomain(Device device, @MappingTarget JpaDeviceEntity entity);
 
+    @Mapping(source = "id", target = "id")
     @Mapping(source = "name", target = "name")
     @Mapping(source = "brand", target = "brand")
     @Mapping(source = "state", target = "state")
     @Mapping(source = "creationDate", target = "creationDate", dateFormat = "yyyy-MM-dd HH:mm:ss")
     DeviceResponseDTO fromDeviceToDeviceResponseDTO(Device device);
+
+    @ObjectFactory
+    default Device createDevice(JpaDeviceEntity jpaDeviceEntity, @TargetType Class<Device> targetType) {
+        return Device.reconstruct(
+                jpaDeviceEntity.getId(),
+                jpaDeviceEntity.getName(),
+                jpaDeviceEntity.getBrand(),
+                jpaDeviceEntity.getState(),
+                jpaDeviceEntity.getCreationDate()
+        );
+    }
+
+
+
 
 
 
