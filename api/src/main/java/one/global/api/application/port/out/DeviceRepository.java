1742bc4c8c727676e5da157e0f31c41129812cca
gibranmenezes <gibran.menezes@seventh.com.br>
2025-07-15 09:14:48 -0300

    refact: modifying return of methods

--- api/src/main/java/one/global/api/application/port/out/DeviceRepository.java
+++ api/src/main/java/one/global/api/application/port/out/DeviceRepository.java
@@ -2,13 +2,12 @@ package one.global.api.application.port.out;
 
 import one.global.api.domain.enums.State;
 import one.global.api.domain.model.Device;
-import one.global.api.presentation.dto.PaginatedResponse;
+import one.global.api.web.dto.PaginatedResponse;
 
 public interface DeviceRepository {
 
     Device save(Device device);
     Device findById(Long id);
     PaginatedResponse<Device> findAll(String brand, State state, int page, int size);
-    void update(Long id, Device device);
     void delete(Long id);
 }
